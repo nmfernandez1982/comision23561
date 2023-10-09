58b0110b8e06f6847a13ecb3f8b309f65b646b22
nicolasfernandez4 <nicolas.fernandez4@bue.edu.ar>
2023-10-09 18:25:54 -0300

    Clase19

---
+++ Clase19/js/calculadora.js
@@ -0,0 +1,11 @@
+function calcular()
+{
+    var valor1=document.getElementById("valor1").value;
+    var valor2=document.getElementById("valor2").value;
+
+    var select=document.getElementById("operacion");
+    var select_activo=select.options[select.selectedIndex].value;
+
+    //console.log("EL valor 1 es:"+valor1+ " El valor 2 es: "+valor2);
+
+}
---
+++ Clase19/js/funciones.js
@@ -0,0 +1,23 @@
+//Por su ID
+var cabecera=console.log(document.getElementById("cabecera"));
+
+//por su Class
+console.log(document.getElementsByClassName("border"));
+
+//por su tag
+console.log(document.getElementsByTagName("main"));
+
+//por su name
+console.log(document.getElementsByName("cabecera"));
+
+//-----------------------------------------------------
+//Clase
+console.log(document.querySelectorAll(".border"));
+//Etiqueta
+console.log(document.querySelectorAll("li"));
+//id
+console.log(document.querySelectorAll("#cabecera"));
+//Name
+console.log(document.querySelectorAll("[name=cabecera]"));
+
+//---------------------------------------------------------
