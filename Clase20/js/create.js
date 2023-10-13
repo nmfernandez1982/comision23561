9b411065027246fa6f5ed7937c974e3267bbc61a
nicolasfernandez4 <nicolas.fernandez4@bue.edu.ar>
2023-10-12 22:14:05 -0300

    Clase20

---
+++ Clase20/js/create.js
@@ -0,0 +1,10 @@
+
+// creo elementos en variables (Memorio)
+var titulo=document.createElement('h2');
+const titulo2=document.createElement('h6');
+var texto=document.createTextNode('Hola Mundo.......');
+var comentario=document.createComment("Desde Aqui el Nav");
+
+var articulo=document.querySelector('article');
+var articulo2 = articulo.cloneNode(true);
+var articulo3 = articulo.cloneNode(true);
